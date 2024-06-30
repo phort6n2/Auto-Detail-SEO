a2bf55b802e7ab81a1f5bfca055e64dff537901e
phort6n2 <matt.lubbes@gmail.com>
2024-06-30 16:41:29 -0600

    s

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,20 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Auto Detail SEO",
+    "email": "matt@coloradospringswebdesigns.com",
+    "phoneForTel": "888-349-4844",
+    "phoneFormatted": "(888) 349-4844",
     "address": {
         "lineOne": "First Address Line",
         "lineTwo": "Second Address Line",
-        "city": "Denver",
+        "city": "Colorado Springs",
         "state": "CO",
-        "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "zip": "80917",
+        "mapLink": "https://www.google.com/maps/place/Colorado+Springs+Web+Designs/@38.9026148,-104.7362115,15z/data=!4m6!3m5!1s0x871349f8b116c977:0x3d7c7aeda65dac17!8m2!3d38.9026148!4d-104.7362115!16s%2Fg%2F11y3ckmpmt?hl=en&entry=ttu"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://autodetailseo.com"
 };
